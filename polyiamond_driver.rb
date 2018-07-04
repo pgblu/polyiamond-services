9c8a43c7bdd83e9c89c1a8f3633942f875b7b3af
Philipp Blume <pgbluster@gmail.com>
2018-07-04 10:41:13 -0500

    copy of turtle code from python folder

--- polyiamond_driver.rb
+++ polyiamond_driver.rb
@@ -20,3 +20,24 @@ HEPTIAMONDS = grow_all(HEXIAMONDS, polyiamond_set)
 puts "number of heptiamonds: #{HEPTIAMONDS.count}\npolyiamond_set: #{polyiamond_set.count}"
 OCTIAMONDS = grow_all(HEPTIAMONDS, polyiamond_set)
 puts "number of octiamonds: #{OCTIAMONDS.count}\npolyiamond_set: #{polyiamond_set.count}"
+
+puts
+puts
+[TRIAMONDS, TETRIAMONDS, PENTIAMONDS, HEXIAMONDS, HEPTIAMONDS, OCTIAMONDS].each do |collection|
+  collection.shuffle.display
+  puts
+end
+
+MY_OCTIAMONDS = []
+puts;puts
+PENTIAMONDS.each_with_index do |shape, idx|
+
+  puts idx + 1
+  MY_OCTIAMONDS[idx] = grow_all(grow_all(cyclic_grow(shape, Set.new()), Set.new()), Set.new())
+  puts "Octiamonds grown from #{shape}: "
+  MY_OCTIAMONDS[idx].display
+  puts
+  puts "There are a total of #{MY_OCTIAMONDS[idx].count} octiamonds grown from this pentiamond"
+  puts
+  puts
+end
