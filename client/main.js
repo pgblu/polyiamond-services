15556cfd21e41d2439573e1c62933bc51dc56d8d
Philipp Blume <pgbluster@gmail.com>
2018-07-04 14:44:25 -0500

    javascript determines positioning, first commit

---
+++ client/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.tgl-1').css({'left': '15px', 'top': '30px'});
+  $('.tgl-2').css({'left': '45px', 'top': '30px'});
+  $('.tgl-3').css({'left': '75px', 'top': '30px'});
+  $('.tgl-4').css({'left': '30px', 'top': '30px'});
+  $('.tgl-5').css({'left': '60px', 'top': '30px'});
+  $('.tgl-6').css({'left': '90px', 'top': '30px'});
+});
+
+//compose static triangles
+//add mobile triangles
+//move mobile triangle
+//replace mobile triangle with new positional data
